7ce151028a4091aeb80ff6c5fcf6bebdb084f66c
paragshelke <paragshelke701@gmail.com>
2025-07-01 19:41:16 +0530

    Add files via upload

---
+++ Temp/vowel.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+void main()
+{
+	char = c;
+	printf("enter a character");
+	scanf("%c",c);
+	
+	if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||c=='A'||c=='E'||c=='I'||c=='O'||c=='u')
+	
+	{
+		if(	if(c>='A'&&c<='Z')||	if(c>='a'&&c<='z'))
+	}
+}
